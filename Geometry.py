b5e50693489fb2aae2d74f50e13c36bfb7b1df02
atticuscull <atticus.cull@gmail.com>
2023-03-27 01:37:23 -0700

    rename Vector3 to Geometry, and add Quaternion

--- Geometry.py
+++ Geometry.py
@@ -1,4 +1,32 @@
 from math import acos, sqrt
+from typing import *
+
+
+class Quaternion:
+    x: float
+    y: float
+    z: float
+    w: float
+
+    def __init__(self, x, y, z, w):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w)
+
+    def __mul__(self, other):
+        return Quaternion(
+            self.x * other.x - self.y * other.y - self.z * other.z - self.w * other.w,
+            self.x * other.y + self.y * other.x + self.z * other.w - self.w * other.z,
+            self.x * other.z + self.z * other.x + self.w * other.y - self.y * other.w,
+            self.x * other.w + self.w * other.x + self.y * other.z - self.z * other.y
+        )
+
+    def conjugate(self):
+        return Quaternion(self.x, -self.y, -self.z, -self.w)
 
 
 class Vector3:
@@ -33,10 +61,11 @@ class Vector3:
     def angle(self, other):
         return acos(self.dot(other) / sqrt(self.sqr_mag() * other.sqr_mag()))
 
-    def __add__(self, other):
-        if type(other) is not Vector3:
-            raise TypeError
+    def rotate(self, quat: Quaternion):
+        quaternion_form = quat * Quaternion(0, self.x, self.y, self.z) * quat.conjugate()
+        return Vector3(quaternion_form.y, quaternion_form.z, quaternion_form.w)
 
+    def __add__(self, other):
         return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def __mul__(self, scalar):
