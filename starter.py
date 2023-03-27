22ce66d039709551a26228932b4654f62ed602ae
atticuscull <atticus.cull@gmail.com>
2023-03-27 00:03:00 -0700

    fix starter.py

--- starter.py
+++ starter.py
@@ -5,7 +5,7 @@ from bsor.Bsor import make_bsor
 with open('sample.bsor', 'rb') as f:
     bsor = make_bsor(f)
 
-ys = [frame.left_hand.position[1] for frame in bsor.frames]
+ys = [frame.left_hand.y for frame in bsor.frames]
 
 plt.plot(ys[1:3000])
 plt.show()
