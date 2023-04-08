ec60c75aa2632fe5ba833934ec20461c4fb9dff1
atticuscull <atticus.cull@gmail.com>
2023-04-08 13:52:56 -0700

    implement from_forward_and_up

--- Geometry.py
+++ Geometry.py
@@ -34,6 +34,39 @@ class Quaternion:
     def dot(self, q):
         return self.x * q.x + self.y * q.y + self.z * q.z + self.w * q.w
 
+    # https://d3cw3dd2w32x2b.cloudfront.net/wp-content/uploads/2015/01/matrix-to-quat.pdf
+    @staticmethod
+    def from_rotation_matrix(M):
+        if M[2][2] < 0:
+            if M[0][0] > M[1][1]:
+                t = 1 + M[0][0] - M[1][1] - M[2][2]
+                q = Quaternion(t, M[0][1] + M[1][0], M[2][0] + M[0][2], M[1][2] - M[2][1])
+
+            else:
+                t = 1 - M[0][0] + M[1][1] - M[2][2]
+                q = Quaternion(M[0][1] + M[1][0], t, M[1][2] + M[2][1], M[2][0] - M[0][2])
+
+        else:
+            if M[0][0] < -M[1][1]:
+                t = 1 - M[0][0] - M[1][1] + M[2][2]
+                q = Quaternion(M[2][0] + M[0][2], M[1][2] + M[2][1], t, M[0][1] - M[1][0])
+
+            else:
+                t = 1 + M[0][0] + M[1][1] + M[2][2]
+                q = Quaternion(M[1][2] - M[2][1], M[2][0] - M[0][2], M[0][1] - M[1][0], t)
+        return q * 0.5 / sqrt(t)
+
+    @staticmethod
+    def from_forward_and_up(forward, up):
+        x_axis = up.cross(forward)
+        y_axis = up
+        z_axis = forward
+        return Quaternion.from_rotation_matrix((
+            (x_axis.x, y_axis.x, z_axis.x),
+            (x_axis.y, y_axis.y, z_axis.y),
+            (x_axis.z, y_axis.z, z_axis.z),
+        ))
+
     @staticmethod
     def Slerp(q0, q1, u):
         θ = acos(q0.dot(q1))
